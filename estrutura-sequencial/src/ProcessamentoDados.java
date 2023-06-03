45e0cea0fc818ed29a65b41ad1a9e3871c9e85e4
beck <lipeho@hotmail.com>
2023-06-02 22:05:22 -0300

    Exemplos de processamento de dados

---
+++ estrutura-sequencial/src/ProcessamentoDados.java
@@ -0,0 +1,16 @@
+public class ProcessamentoDados {
+
+    public static void main(String[] args){
+        double b, B, h, area;
+        b = 6.0;
+        B = 5.0;
+        h = 2.0;
+
+        area = (b + B) / 2.0 * h;
+            System.out.println(area);
+
+
+    }
+
+    
+}
