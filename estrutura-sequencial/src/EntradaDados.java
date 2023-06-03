3215cf011c8e89d9a89ac9e4b03ea3c5c3ea45dd
beck <lipeho@hotmail.com>
2023-06-02 22:54:06 -0300

    Exercicios

---
+++ estrutura-sequencial/src/EntradaDados.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class EntradaDados {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        double x;
+        int y;
+        String z;
+        x = sc.nextDouble();
+        y = sc.nextInt();
+        z = sc.next();
+        System.out.println("Dados digitados: ");
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+
+
+
+
+
+
+        sc.close();
+
+
+
+    }
+    
+}
